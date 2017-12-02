f30da9e39b307d1607712601e2e0cec2ca2ddfae
Yuchen Zhong <yzhong.cs@gmail.com>
2017-12-02 10:12:30 -0500

    For those who want to do everything in code instead of interface builder.

--- AutoSizeStackview/ViewController.swift
+++ AutoSizeStackview/ViewController.swift
@@ -8,15 +8,7 @@
 
 import UIKit
 
-class TableviewCell: UITableViewCell {
-    @IBOutlet var stackView: UIStackView!
-}
-
-class ViewController: UIViewController {
-    @IBOutlet var tableView: UITableView!
-}
-
-extension ViewController: UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -26,12 +18,26 @@ extension ViewController: UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TableviewCell", for: indexPath) as! TableviewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableviewCell", for: indexPath)
+
+        let stackview = UIStackView()
+        stackview.translatesAutoresizingMaskIntoConstraints = false
+        stackview.axis = .vertical
+        cell.addSubview(stackview)
+        NSLayoutConstraint.activate([
+            stackview.leadingAnchor.constraint(equalTo: cell.leadingAnchor, constant: 10),
+            stackview.trailingAnchor.constraint(equalTo: cell.trailingAnchor, constant: -10),
+            stackview.topAnchor.constraint(equalTo: cell.topAnchor, constant: 10),
+            stackview.bottomAnchor.constraint(equalTo: cell.bottomAnchor, constant: -10),
+            ])
+
         for i in 1...indexPath.row + 1 {
             let label = UILabel()
             label.text = "Row \(indexPath.row), Label \(i)"
-            cell.stackView.addArrangedSubview(label)
+            stackview.addArrangedSubview(label)
         }
+
+
         return cell
     }
 }
