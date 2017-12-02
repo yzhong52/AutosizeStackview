b517fec69fc79c6e1260d545af5b414e4256439c
Yuchen Zhong <yzhong.cs@gmail.com>
2017-12-02 09:25:17 -0500

    Demo: autosize stack view inside table view

--- AutoSizeStackview/ViewController.swift
+++ AutoSizeStackview/ViewController.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+class TableviewCell: UITableViewCell {
+    @IBOutlet var stackView: UIStackView!
 }
 
+class ViewController: UIViewController {
+    @IBOutlet var tableView: UITableView!
+}
+
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableviewCell", for: indexPath) as! TableviewCell
+        for i in 1...indexPath.row + 1 {
+            let label = UILabel()
+            label.text = "Row \(indexPath.row), Label \(i)"
+            cell.stackView.addArrangedSubview(label)
+        }
+        return cell
+    }
+}
