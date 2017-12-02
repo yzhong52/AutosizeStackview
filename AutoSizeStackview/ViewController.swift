9f4baf342dfa0dc77e720a922e3d1c94c6e856b2
Yuchen Zhong <yzhong.cs@gmail.com>
2017-12-02 09:08:33 -0500

    Initial Commit

---
+++ AutoSizeStackview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoSizeStackview
+//
+//  Created by Yuchen Zhong on 2017-12-02.
+//  Copyright © 2017 Yuchen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
